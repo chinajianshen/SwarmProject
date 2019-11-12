a2574057ed6edd32f87591da81ffd71c86f1c76d
chinajianshen <281349838@qq.com>
2019-11-12 13:31:10 +0800

    首次创建项目

---
+++ Code/Server/JianShen.Swarm.Web/JianShen.Swarm.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JianShen.Swarm.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
