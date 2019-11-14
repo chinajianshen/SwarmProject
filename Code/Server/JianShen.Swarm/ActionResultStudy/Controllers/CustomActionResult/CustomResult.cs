1cbd4d6557510bd14330a0b0395a7a0c04f4dce9
chinajianshen <281349838@qq.com>
2019-11-14 17:54:27 +0800

    自定义ActionResult

---
+++ Code/Server/JianShen.Swarm/ActionResultStudy/Controllers/CustomActionResult/CustomResult.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ActionResultStudy.Controllers.CustomActionResult
+{
+    public class CustomResult:ActionResult
+    {
+        public object Value { get; private set; }
+
+        public CustomResult(object value)
+        {
+            this.Value = value;
+        }
+
+        /// <summary>
+        /// Result执行者
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public override async Task ExecuteResultAsync(ActionContext context)
+        {
+            //return base.ExecuteResultAsync(context);
+            var services = context.HttpContext.RequestServices;
+            var executor = services.GetRequiredService<IActionResultExecutor<CustomResult>>();
+            await executor.ExecuteAsync(context, new CustomResult(this));
+
+        }
+    }
+
+    public class CustomResultExecutor<T> : IActionResultExecutor<T> where T : CustomResult
+    {
+        /// <summary>
+        /// 执行
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="result"></param>
+        /// <returns></returns>
+        public async Task ExecuteAsync(ActionContext context, T result)
+        {
+            var valueString = JsonConvert.SerializeObject(result.Value);
+            context.HttpContext.Response.ContentType = "Content-Type:text/html;charset=utf-8";
+            await context.HttpContext.Response.WriteAsync(valueString);
+;        }
+    }
+}
---
+++ Code/Server/JianShen.Swarm/ActionResultStudy/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ActionResultStudy.Models;
+using ActionResultStudy.Controllers.CustomActionResult;
+
+namespace ActionResultStudy.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Get()
+        {
+            return new CustomResult(new
+            {
+                Data = new { a = 1, B = "bbb", C = DateTime.Now, d = new string[] { "a", "b", "c" } },
+                Status = true,
+                Message = "成功"
+            });
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
