9f8daae9fbd7d7a5e8bcdedc85c0c460dd2ef503
chinajianshen <281349838@qq.com>
2019-11-15 17:20:16 +0800

    VS2019 创建VUE单页应用和创建前后端分离VUE单页应用

---
+++ Code/Server/JianShen.Swarm/NetCoreMvcSPA2/ClientApp/App.vue
@@ -0,0 +1,21 @@
+﻿<template>
+    <div id="app">
+        <h2>Hello Vue. {{message}}</h2>
+        <h2>Hello Vue. {{message2}}</h2>
+        <h2>Hello Vue. {{message3}}</h2>
+        <h2>Hello Vue. {{message4}}</h2>
+    </div>
+</template>
+<script>
+  export default {
+    name: 'app',
+    data: function () {
+      return {
+          message: "ok.",
+          message2: "ok2.",
+          message3:"ok3.",
+          message4:"ok4.",
+      }
+    }
+  }
+</script>
---
+++ Code/Server/JianShen.Swarm/NetCoreMvcSPA2/webpack.config.js
@@ -0,0 +1,30 @@
+﻿const path = require('path');
+const bundleOutputDir = './wwwroot/dist';
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+    resolve: {
+        extensions: ['.js', '.vue']
+    },
+    entry: './ClientApp/main.js',
+    module: {
+        rules: [
+            { test: /\.vue$/, include: /ClientApp/, loader: 'vue-loader' }
+        ]
+    },
+    output: {
+        path: path.join(__dirname, bundleOutputDir),
+        filename: '[name].js',
+        publicPath: 'dist/'
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ],
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),// 配置开发服务运行时的文件根目录
+        host: 'localhost',// 开发服务器监听的主机地址
+        compress: true,   // 开发服务器是否启动gzip等压缩
+        port: 5000        // 开发服务器监听的端口
+    }
+    
+}
